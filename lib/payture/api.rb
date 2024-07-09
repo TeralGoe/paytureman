cbaa5419679cfbd987c6b8799c8d310e3fa211a3
Yana Arkhipova <frontend.ark@gmail.com>
2024-07-09 16:09:57 +0300

    test CGI.escape III

--- lib/payture/api.rb
+++ lib/payture/api.rb
@@ -13,7 +13,7 @@ module Paytureman
       data = { SessionType: :Pay, OrderId: order_id, Amount: amount, IP: ip }
       data.merge!(description)
       init_params = {
-          'Data' => CGI.escape(data.map { |k, v| "#{k.to_s.camelize}=#{v}" }.join(';'))
+          'Data' => CGI.escape(data.map).gsub("+", "%20")
       }
 
       response = make_request(:init, init_params)
