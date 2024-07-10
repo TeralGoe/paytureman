0cff21f815b7a4faf57dee15bfe24ef2c06c4730
Yana Arkhipova <frontend.ark@gmail.com>
2024-07-10 21:31:35 +0300

    test URI.encode_www_form I

--- lib/payture/api.rb
+++ lib/payture/api.rb
@@ -13,7 +13,7 @@ module Paytureman
       data = { SessionType: :Pay, OrderId: order_id, Amount: amount, IP: ip }
       data.merge!(description)
       init_params = {
-          'Data' => CGI::escape(data.map { |k, v| "#{k.to_s.camelize}=#{v}" }.join(';'))
+          'Data' => URI.encode_www_form(data)
       }
 
       response = make_request(:init, init_params)
