d00f8c7ec00ef48aa17b85fefbeff5c5111d7c38
Yana Arkhipova <frontend.ark@gmail.com>
2024-07-10 20:55:12 +0300

    test CGI.escapeElement I

--- lib/payture/api.rb
+++ lib/payture/api.rb
@@ -13,7 +13,7 @@ module Paytureman
       data = { SessionType: :Pay, OrderId: order_id, Amount: amount, IP: ip }
       data.merge!(description)
       init_params = {
-          'Data' => CGI.escapeURIComponent(data.map { |k, v| "#{k.to_s.camelize}=#{v}" }.join(';'))
+          'Data' => CGI.escapeElement(data.map { |k, v| "#{k.to_s.camelize}=#{v}" }.join(';'))
       }
 
       response = make_request(:init, init_params)
